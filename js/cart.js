d62d7b706c10129ad9b28f94a3f0df4ba3b016cf
purplelightning <1979506141@qq.com>
2017-10-14 16:56:20 +0800

    过滤器的使用 商品数量的点击事件

--- js/cart.js
+++ js/cart.js
@@ -19,14 +19,37 @@ var vm = new Vue({
       //   _this.totalMoney = res.data.result.totalMoney;
       // });
       this.$http.get('data/cartData.json', {'id': 123}).then(function (res) {
-          _this.productList = res.data.result.list;
-          _this.totalMoney = res.data.result.totalMoney;
-        }
-      )
+        _this.productList = res.data.result.list;
+        _this.totalMoney = res.data.result.totalMoney;
+      })
+    },
+    changeMoney: function (pro, way) {
+      if (way < 0) {
+        pro.productQuantity--;
+      } else {
+        pro.productQuantity++;
+      }
+      if (pro.productQuantity < 1) {
+        pro.productQuantity = 1;
+      }
     }
   },
-
+  //局部过滤器
+  filters: {
+    formatMoney: function (value) {
+      return '￥ ' + value.toFixed(2);
+    }
+  }
 });
-Vue.filter = function (value) {
-  return value + " 元";
-};
+//全局过滤器
+Vue.filter('money', function (value, type) {
+  return value.toFixed(2) + type;
+});
+
+
+
+
+
+
+
+
