72437e0396c2d479186faa1659831eac8e5bbb4e
purplelightning <1979506141@qq.com>
2017-10-14 21:17:39 +0800

    address页面 computed计算属性的使用,展示一定数量的item 卡片的选择 默认地址的设置

--- js/address.js
+++ js/address.js
@@ -3,7 +3,9 @@ new Vue({
   data: {
     addressList: [],
     showSize: 3,
-
+    count: 0,
+    shippingMethod: 1,
+    currentIndex: 0,
   },
   mounted: function () {
     this.$nextTick(function () {
@@ -11,6 +13,7 @@ new Vue({
     })
   },
   methods: {
+    //获取地址
     getAddressList: function () {
       var _this = this;
       this.$http.get('data/address.json').then(function (response) {
@@ -20,12 +23,33 @@ new Vue({
         }
 
       })
+    },
+    //显示/收起更多地址
+    showMoreItem: function () {
+      if (this.count % 2 == 0) {//判断显示还是收起,与jquery中的toggle一样
+        this.showSize = this.addressList.length;
+      } else {
+        this.showSize = 3;
+      }
+      this.count++;
+      this.getAddressList();
+    },
+    //设置默认地址
+    setDefault: function (addressId) {
+      this.addressList.forEach(function (address, index) {
+        if (address.addressId == addressId) {
+          address.isDefault = true;
+        } else {
+          address.isDefault = false;
+        }
+      })
     }
   },
-  computed: {},
-  filter: {
-    showAmount: function (nums, showSize) {
-      return nums.slice(0, showSize);
+  computed: {
+    //展示一定数目的地址
+    filterAddress: function () {
+      return this.addressList.slice(0, this.showSize);
     }
-  }
+  },
+
 });
