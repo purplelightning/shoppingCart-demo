d5553e4e78385053c0cf56bb87a42276a6e9ef76
purplelightning <1979506141@qq.com>
2017-10-15 15:46:54 +0800

    订单信息页面修改

--- js/checkOrder.js
+++ js/checkOrder.js
@@ -1,23 +1,29 @@
 new Vue({
-  el:'#order',
-  data:{
-    orderList:[],
-
+  el: '#order',
+  data: {
+    orderList: [],
+    totalOrder: 0,
   },
-  mounted:function () {
+  mounted: function () {
     this.$nextTick(function () {
       this.getOrderList();
     })
   },
-  methods:{
-    getOrderList:function () {
-      var _this=this;
+  methods: {
+    getOrderList: function () {
+      var _this = this;
       this.$http.get('data/cartData.json').then(function (res) {
-        _this.orderList=res.data.result.list;
+        _this.orderList = res.data.result.list;
+        _this.calcOrderMoney();
+      })
+    },
+    calcOrderMoney: function () {
+      var _this = this;
+      _this.totalOrder = 0;
+      this.orderList.forEach(function (item, index) {
+        _this.totalOrder += item.productQuantity * item.productPrice;
       })
     }
   },
-  computed:{
-
-  }
+  computed: {}
 });
